6e24b8905abf602f1e924b0542c5809ac365dbff
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2023-12-07 15:40:57 -0500

    Styled Active Link in NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+'use client';
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
-import { FaHeadphones } from "react-icons/fa6";
+import { FaFaceLaughWink, FaHeadphones } from "react-icons/fa6";
 
 
 const NavBar = () => {
@@ -8,11 +11,18 @@ const links =[
     {label: "Dashboard", href:'/'},
     {label: "Issues", href:'/issues'}
 ]
+const currentPath = usePathname();
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href='/'><FaHeadphones /></Link>
         <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} className='text-slate-500 hover:text-slate-900 transition-colors' href={link.href}>{link.label}</Link> )}
+            {links.map(link => <Link key={link.href} className={
+              classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href!== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+
+            })} href={link.href}>{link.label}</Link> )}
         </ul>
     </nav>
   )
